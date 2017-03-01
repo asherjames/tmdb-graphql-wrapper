f8c37e11bbdba28a4ba57f70dc9615e16420a338
asherjames <asherjames.nz@gmail.com>
2017-03-01 22:06:53 +1300

    Add unit tests for alternative queries

--- src/test/java/GenreQueryTest.java
+++ src/test/java/GenreQueryTest.java
@@ -9,28 +9,61 @@ import static org.assertj.core.api.Assertions.*;
 
 public class GenreQueryTest {
 
-    private static Object resultObject;
-    private static JsonObject resultJson;
+    private static Object resultObjectIdName;
+    private static JsonObject resultJsonIdName;
+    private static Object resultObjectId;
+    private static JsonObject resultJsonId;
+    private static Object resultObjectName;
+    private static JsonObject resultJsonName;
 
     @BeforeClass
     public static void setupResults() {
-        resultObject = GenreObjectTypes.executeGenreQuery("{genres{id name}}");
-        resultJson = new Gson().toJsonTree(resultObject).getAsJsonObject();
+        Gson gson = new Gson();
+
+        resultObjectIdName = GenreObjectTypes.executeGenreQuery("{genres{id name}}");
+        resultJsonIdName = gson.toJsonTree(resultObjectIdName).getAsJsonObject();
+
+        resultObjectId = GenreObjectTypes.executeGenreQuery("{genres{id}}");
+        resultJsonId = gson.toJsonTree(resultObjectId).getAsJsonObject();
+
+        resultObjectName = GenreObjectTypes.executeGenreQuery("{genres{name}}");
+        resultJsonName = gson.toJsonTree(resultObjectName).getAsJsonObject();
     }
 
     @Test
     public void correctQueryShouldNotReturnNull() {
-        assertThat(resultObject).isNotNull();
+        assertThat(resultObjectIdName).isNotNull();
     }
 
     @Test
     public void correctQueryShouldReturnCorrectNumberOfGenres() {
-        assertThat(resultJson.get("genres").getAsJsonArray().size()).isEqualTo(19);
+        assertThat(resultJsonIdName.get("genres").getAsJsonArray().size()).isEqualTo(19);
     }
 
     @Test
     public void correctQueryShouldReturnListWithCorrectValues() {
-        assertThat(resultJson.get("genres").getAsJsonArray().get(0).getAsJsonObject().get("id"))
+        assertThat(resultJsonIdName.get("genres").getAsJsonArray().get(0).getAsJsonObject().get("id"))
                 .isEqualTo(new JsonPrimitive(28));
+
+        assertThat(resultJsonIdName.get("genres").getAsJsonArray().get(0).getAsJsonObject().get("name"))
+                .isEqualTo(new JsonPrimitive("Action"));
+    }
+
+    @Test
+    public void correctIdQueryReturnsListWithJustIds() {
+        assertThat(resultJsonId.get("genres").getAsJsonArray().get(0).getAsJsonObject().get("id"))
+                .isEqualTo(new JsonPrimitive(28));
+
+        assertThat(resultJsonId.get("genres").getAsJsonArray().get(0).getAsJsonObject().get("name"))
+                .isNull();
+    }
+
+    @Test
+    public void correctNameQueryReturnsListWithJustNames() {
+        assertThat(resultJsonName.get("genres").getAsJsonArray().get(0).getAsJsonObject().get("id"))
+                .isNull();
+
+        assertThat(resultJsonName.get("genres").getAsJsonArray().get(0).getAsJsonObject().get("name"))
+                .isEqualTo(new JsonPrimitive("Action"));
     }
 }
