cd0b9d0153d5882807f8bb9f99e33cecea146e9d
asherjames <asherjames.nz@gmail.com>
2017-03-11 23:07:10 +1300

    Add extra logic to ApiKeyManager

--- src/main/java/ash/java/graphql/ApiKeyManager.java
+++ src/main/java/ash/java/graphql/ApiKeyManager.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ApiKeyManager {
@@ -15,14 +16,15 @@ public class ApiKeyManager {
     private ApiKeyManager() {
         try {
             log.info("Attempting to load API key from properties file...");
-
-            props.load(this.getClass().getClassLoader().getResourceAsStream("apiKey.properties"));
+            InputStream propertiesFile = this.getClass().getClassLoader().getResourceAsStream("apiKey.properties");
+            if(propertiesFile == null) {
+                throw new IOException("Could not find properties file!");
+            }
+            props.load(propertiesFile);
             apiKeyString = props.getProperty("apikey");
         } catch (IOException e) {
-            log.error("Could not load properties file!  Attempting to load from environment variable...", e);
-
+            log.info("Attempting to load API key from environment variables...", e);
             apiKeyString = System.getenv("TMDB_API_KEY");
-
             if(apiKeyString == null) {
                 throw new TmdbGqlException("Could not load API key from environment variable!");
             }
