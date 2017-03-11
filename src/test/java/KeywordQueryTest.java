c178c47257070908e576ddba6dff5155e8eddc20
asherjames <asherjames.nz@gmail.com>
2017-03-11 17:11:04 +1300

    Add unit tests for keyword query variations

--- src/test/java/KeywordQueryTest.java
+++ src/test/java/KeywordQueryTest.java
@@ -1,6 +1,7 @@
 import ash.java.graphql.schema.KeywordObjectTypes;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -10,6 +11,10 @@ public class KeywordQueryTest {
 
     private static Object resultObject;
     private static JsonObject resultJson;
+    private static Object resultIdObject;
+    private static JsonObject resultIdJson;
+    private static Object resultNameObject;
+    private static JsonObject resultNameJson;
 
     @BeforeClass
     public static void setupResults() {
@@ -17,6 +22,12 @@ public class KeywordQueryTest {
 
         resultObject = KeywordObjectTypes.executeKeywordQuery("{keywordList(filmId: \"123\"){id name}}");
         resultJson = gson.toJsonTree(resultObject).getAsJsonObject();
+
+        resultIdObject = KeywordObjectTypes.executeKeywordQuery("{keywordList(filmId: \"123\"){id}}");
+        resultIdJson = gson.toJsonTree(resultIdObject).getAsJsonObject();
+
+        resultNameObject = KeywordObjectTypes.executeKeywordQuery("{keywordList(filmId: \"123\"){name}}");
+        resultNameJson = gson.toJsonTree(resultNameObject).getAsJsonObject();
     }
 
     @Test
@@ -27,5 +38,33 @@ public class KeywordQueryTest {
     @Test
     public void correctQueryShouldReturnJsonArrayOfCorrectLength(){
         assertThat(resultJson.get("keywordList").getAsJsonArray().size()).isEqualTo(4);
+    }
+
+    @Test
+    public void correctQueryShouldReturnListWithCorrectKeywords(){
+        assertThat(resultJson.get("keywordList").getAsJsonArray().get(0).getAsJsonObject().get("name"))
+                .isEqualTo(new JsonPrimitive("elves"));
+
+        assertThat(resultJson.get("keywordList").getAsJsonArray().get(0).getAsJsonObject().get("id"))
+                .isEqualTo(new JsonPrimitive(603));
+    }
+
+    @Test
+    public void correctIdQueryShouldReturnListWithJustIds(){
+        assertThat(resultIdJson.get("keywordList").getAsJsonArray().get(3).getAsJsonObject().get("id"))
+                .isEqualTo(new JsonPrimitive(10364));
+
+        assertThat(resultIdJson.get("keywordList").getAsJsonArray().get(0).getAsJsonObject().get("name"))
+                .isNull();
+
+    }
+
+    @Test
+    public void correctNameQueryReturnsListWithJustNames() {
+        assertThat(resultNameJson.get("keywordList").getAsJsonArray().get(0).getAsJsonObject().get("id"))
+                .isNull();
+
+        assertThat(resultNameJson.get("keywordList").getAsJsonArray().get(2).getAsJsonObject().get("name"))
+                .isEqualTo(new JsonPrimitive("hobbit"));
     }
 }
