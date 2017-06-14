dd4fb2b6e1b01f739eb55c4c4ecb9153367e449f
asherjames <asherjames.nz@gmail.com>
2017-06-14 22:45:27 +1200

    Add unit test for nested airDate

--- src/test/java/ash/java/graphql/test/schemas/TvSeasonSearchSchemaTest.java
+++ src/test/java/ash/java/graphql/test/schemas/TvSeasonSearchSchemaTest.java
@@ -5,9 +5,9 @@ import ash.java.graphql.data.TvDao;
 import ash.java.graphql.fields.FieldProducer;
 import ash.java.graphql.fields.TvSeasonSearchSchema;
 import ash.java.graphql.test.TestUtil;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
+import com.google.gson.*;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -23,6 +23,7 @@ public class TvSeasonSearchSchemaTest {
     private static Gson gson = new Gson();
 
     private static JsonObject seasonIdJson;
+    private static JsonObject episodeAirDateJson;
 
     @BeforeClass
     public static void setupResults() {
@@ -30,11 +31,23 @@ public class TvSeasonSearchSchemaTest {
 
         Object seasonIdQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){id}}");
         seasonIdJson = TestUtil.extractData(seasonIdQueryResultObject);
+
+        Object episodeAirDateQueryResultObject = schema.executeQuery("{tvSeasonSearch(tvId: 1408, seasonNum: 5){episodes{airDate}}}");
+        episodeAirDateJson = TestUtil.extractData(episodeAirDateQueryResultObject);
     }
 
     @Test
     public void seasonIdIsCorrect() {
         assertThat(getResult(seasonIdJson).get("id")).isEqualTo(new JsonPrimitive(3676));
+    }
+
+    @Test
+    public void episodeAirdatesAreCorrect() {
+        JsonElement datesJson = episodeAirDateJson.getAsJsonObject("tvSeasonSearch").get("episodes");
+        List<AirDateMapping> dates = gson.fromJson(datesJson, new TypeToken<List<AirDateMapping>>(){}.getType());
+
+        assertThat(dates).hasSize(24);
+        assertThat(dates.stream().map(m -> m.value)).containsSequence("2008-09-16", "2008-09-23", "2008-09-30", "2008-10-14", "2008-10-21");
     }
 
     private static JsonObject getResult(JsonObject jsonObject) {
@@ -50,5 +63,10 @@ public class TvSeasonSearchSchemaTest {
         fieldProducers.add(new TvSeasonSearchSchema(tvDao));
 
         return fieldProducers;
+    }
+
+    private class AirDateMapping {
+        @SerializedName("airDate")
+        private String value;
     }
 }
